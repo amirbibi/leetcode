875dca266d62c7c83f2b36ba47aac9535a5388e7
Amir Bibi <53130876+amirbibi@users.noreply.github.com>
2024-07-24 19:16:39 +0300

    Update 20.valid-parentheses.py
    Fixed tabs to 2 spaces

--- 20.valid-parentheses.py
+++ 20.valid-parentheses.py
@@ -1,18 +1,17 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
-        h = {
-            ')':'(',
-            ']':'[',
-            '}':'{'
-        }
-        stack = []
-        for c in s:
-            if (c in h):
-                if (len(stack) > 0 and stack[-1] == h[c]):
-                    stack.pop()
-                else:
-                    return False
-            else:
-                stack.append(c)
-        return len(stack) == 0
-        
+  def isValid(self, s: str) -> bool:
+    h = {
+        ')':'(',
+        ']':'[',
+        '}':'{'
+    }
+    stack = []
+    for c in s:
+      if (c in h):
+        if (len(stack) > 0 and stack[-1] == h[c]):
+          stack.pop()
+        else:
+          return False
+      else:
+        stack.append(c)
+    return len(stack) == 0
